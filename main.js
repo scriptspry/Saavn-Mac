2cce9fbb198f82a1927b153246b488b5afc1b082
scriptspry <hi@scriptspry.com>
2016-07-31 00:24:25 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,134 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+
+// Open external urls in default browser
+const open = require('open')
+
+// Module to bind media shortcuts
+const globalShortcut = require('electron').globalShortcut
+
+// Specify flash path, supposing it is placed in the same directory with main.js.
+let pluginName
+
+/*
+// TODO: package .dll and .so as well for making this work with other platforms
+switch (process.platform) {
+  case 'win32':
+    pluginName = 'pepflashplayer.dll'
+    break
+  case 'darwin':
+    pluginName = 'PepperFlashPlayer.plugin'
+    break
+  case 'linux':
+    pluginName = 'libpepflashplayer.so'
+    break
+}
+*/
+
+// Works only for OS X now
+if (process.platform != 'darwin') {
+  dialog.showMessageBox({
+    "type": "error",
+    "title": "Unsupported Platform",
+	"message": "Only for Mac"
+  });
+}
+
+pluginName = 'PepperFlashPlayer.plugin'
+
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName))
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+//	logo: path.join(__dirname, 'logo.png'),
+    webPreferences: {
+      plugins: true,
+      nodeIntegration: false
+    }})
+
+  // And load saavn.com
+  mainWindow.loadURL(`http://www.saavn.com/`)
+
+  // Open DevTools
+  // mainWindow.webContents.openDevTools()
+
+  var webView = mainWindow.webContents;
+
+  // Navigation restricted to mainWindow only
+  var handleUrl = function (e, url) {
+    e.preventDefault()
+
+	// Opening saavn urls
+	if (url.replace('https://','').replace('http://','').indexOf('www.saavn.com') == 0)
+      mainWindow.loadURL(url)
+    else
+      // Open External URLs in the default web browser
+      open(url)
+  }
+  webView.on('will-navigate', handleUrl)
+  webView.on('new-window', handleUrl)
+
+  // Bind Media Shortcuts - This wouldn't have been required if the
+  // client side code listened to these keys along with KEY_NEXT, KEY_SPACE etc.
+  // For now trigger functionality by remapping the keys
+  globalShortcut.register('MediaPlayPause', function () {
+    webView.sendInputEvent({
+      type: 'keyDown',
+      keyCode: 'Space'
+    })
+  })
+
+  globalShortcut.register('MediaNextTrack', function () {
+    webView.sendInputEvent({
+      type: 'keyDown',
+      keyCode: 'Right'
+    })
+  })
+
+  globalShortcut.register('MediaPreviousTrack', function () {
+    webView.sendInputEvent({
+      type: 'keyDown',
+      keyCode: 'Left'
+    })
+  })
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // When quitting, unregister the shortcuts
+  globalShortcut.unregisterAll()
+
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  // if (process.platform !== 'darwin') {
+  app.quit()
+  // }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
